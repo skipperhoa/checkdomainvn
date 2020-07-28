5e104323a4a6a26fcc52488615d26a6383230341
nguyenthanhhoadev <nguyen.thanh.hoa.ctec@gmail.com>
2020-07-28 11:17:54 +0700

    capnhat 12

--- app.js
+++ app.js
@@ -8,33 +8,29 @@ app.set('view engine', 'ejs');
 app.set("views", "./views");
 app.use(express.static('public'))
 /**set server */
-var httpServer = http.createServer(app);
-var io = require('socket.io')(httpServer);
-app.get("/",function(req,res){
-   res.render("index");
-});
 const https = require('https');
 
 
-io.on('connection', (socket) => {
-    socket.on("domain",function(msg){
-        var domain = msg.domain;
-        console.log(domain.length)
-        https.get('http://namkhoa.phongkhamdakhoahongphong.vn/sub-wp/', (res) => {
-            console.log('statusCode:', res.statusCode);
-            console.log('headers:', res.headers);
-            socket.emit("statusDomain",res)
-            res.on('data', (d) => {
-                console.log(d);
-                socket.emit("statusDomain",d)
-            });
+var httpServer = https.createServer(app);
+var io = require('socket.io')(httpServer);
+app.get("/",function(req,res){
+    https.get('http://namkhoa.phongkhamdakhoahongphong.vn/sub-wp/', (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+        res.on('data', (d) => {
+            console.log(d);
 
-        }).on('error', (e) => {
-            console.error(e);
-            socket.emit("statusDomain",e)
         });
 
+    }).on('error', (e) => {
+        console.error(e);
 
+    });
+   res.render("index",{code:res.statusCode});
+});
+
+io.on('connection', (socket) => {
+    socket.on("domain",function(msg){
     })
 });
 httpServer.listen(process.env.PORT || httpPort, () => {
