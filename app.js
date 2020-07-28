d4ee2bf70055f1e80432f0bb1874c328cdc33a38
nguyenthanhhoadev <nguyen.thanh.hoa.ctec@gmail.com>
2020-07-28 11:51:39 +0700

    capnhat 16

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require('express');
 const http = require('http');
 var request = require('request');
 var rp = require('request-promise');
-var sslRedirect = require('heroku-ssl-redirect');
 const httpPort = 8000;
 var app = express()
-app.use(sslRedirect());
 app.set('view engine', 'ejs');
 app.set("views", "./views");
 app.use(express.static('public'))
 /**set server */
-const https = require('https');
-var httpServer = https.createServer(app);
+var httpServer = http.createServer(app);
 var io = require('socket.io')(httpServer);
 app.get("/",function(req,ress){
     ress.render("index");
