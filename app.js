9afbcbf20fe04b49d32b517e9ad723b9da2c71fe
nguyenthanhhoadev <nguyen.thanh.hoa.ctec@gmail.com>
2020-07-28 15:16:57 +0700

    e4

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require('express');
-const http = require('http');
 var request = require('request');
 var rp = require('request-promise');
-
-var app = express()
+const httpPort = 8000;
+/**set server */
+var app = require('express')();
 app.set('view engine', 'ejs');
 app.set("views", "./views");
 app.use(express.static('public'))
-/**set server */
-app.set('port', process.env.PORT || 8000);
-app.get('/', function(req, res) {
-    var status = 0;
-   
-    res.render("index",{code:status})
-});
-//
-// io.on('connection', (socket) => {
-//     socket.on('domain', (msg) => {
-//
-//     });
-// });
+var http = require('http').createServer(app);
 
-app.listen(8000, function () {
-    console.log('***** exp listening on port: 8000');
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
 });
